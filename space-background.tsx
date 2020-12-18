00ccd9681fce174df0f579e358f92183fab5feeb
MisterTechnik <52965686+MisterTechnik@users.noreply.github.com>
2020-12-18 14:25:29 +0100

    Initial upload

---
+++ space-background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.17" name="space-background" tilewidth="32" tileheight="32" tilecount="1023" columns="31">
+ <image source="Bilder/space-background.jpg" width="1000" height="1080"/>
+</tileset>
---
+++ tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.17" name="tileset1" tilewidth="32" tileheight="32" tilecount="450" columns="25">
+ <image source="Bilder/IMG_1695.png" width="800" height="600"/>
+</tileset>
