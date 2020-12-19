bdde7629adfb808479bee6dde99b78cbeaa13d4b
MisterTechnik <finneliasw@gmail.com>
2020-12-19 22:38:48 +0100

    Delete Character.tsx

--- Character.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.12.17" name="Character" tilewidth="32" tileheight="32" tilecount="210" columns="14">
- <image source="Bilder/Character.png" width="470" height="495"/>
-</tileset>
